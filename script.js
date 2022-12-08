6ed41b56acd8a39f341fa10469d1ae7276505c5a
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2022-12-08 16:52:49 +0100

    add menu burger

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("conneceté");
+const icon = document.querySelector(".fa-bars");
+const navListMobile = document.querySelector(".navlist-mobile");
+
+icon.addEventListener("click", function () {
+  navListMobile.classList.toggle("list-visible");
+  icon.classList.toggle("icon-visible");
+});
+
+let extendImage = document.querySelector(".photoSalle");
+let info1 = document.querySelector(".info1");
+let info2 = document.querySelector(".info2");
+if (window.screen.width > "1111") {
+  extendImage.addEventListener("click", function () {
+    extendImage.classList.toggle("photo-extend");
+    info1.classList.toggle("blurzone");
+    info2.classList.toggle("blurzone");
+  });
+}
+
